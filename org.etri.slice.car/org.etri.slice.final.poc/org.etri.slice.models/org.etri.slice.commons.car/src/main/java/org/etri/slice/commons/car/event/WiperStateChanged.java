dd0312165ce7b56f301f3fcf06669af112681835
woosungpil <woosungpil@kaist.ac.kr>
2018-11-20 18:37:09 +0900

    final-poc branch initialization

---
+++ org.etri.slice.car/org.etri.slice.final.poc/org.etri.slice.models/org.etri.slice.commons.car/src/main/java/org/etri/slice/commons/car/event/WiperStateChanged.java
@@ -0,0 +1,30 @@
+package org.etri.slice.commons.car.event;
+
+import org.etri.slice.commons.SliceContext;
+import org.etri.slice.commons.SliceEvent;
+import org.kie.api.definition.type.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import org.etri.slice.commons.car.context.WiperState;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@EqualsAndHashCode(callSuper=false)
+
+@Role(Role.Type.EVENT)
+@SliceContext
+public class WiperStateChanged extends SliceEvent {
+
+	public static final String TOPIC = "wiper_state_changed";
+	private static final long serialVersionUID = -3994249453571390832L;
+	
+	private WiperState wiperstate;
+}
