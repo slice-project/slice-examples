7af37b1718f9e14b20dd590e31baf487cf6bece4
Sungpil Woo <woosungpil@kaist.ac.kr>
2018-11-21 13:31:37 +0900

    first commit sample

---
+++ org.etri.slice.sample/org.etri.slice.models/org.etri.slice.commons.room/src/main/java/org/etri/slice/commons/room/event/TemperatureChanged.java
@@ -0,0 +1,30 @@
+package org.etri.slice.commons.room.event;
+
+import org.etri.slice.commons.SliceContext;
+import org.etri.slice.commons.SliceEvent;
+import org.kie.api.definition.type.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import org.etri.slice.commons.room.context.Temperature;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@EqualsAndHashCode(callSuper=false)
+
+@Role(Role.Type.EVENT)
+@SliceContext
+public class TemperatureChanged extends SliceEvent {
+
+	public static final String TOPIC = "temperate_changed";
+	private static final long serialVersionUID = 919379969629768571L;
+	
+	private Temperature temp;
+}
